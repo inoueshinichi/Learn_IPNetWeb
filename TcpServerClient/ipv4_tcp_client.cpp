e8e7b81db152c8f3dc8aa4e373e7307e68a605a2
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-05-03 16:09:19 +0900

    [Update] OK ipv4 tcp client and server

--- TcpServerClient/ipv4_tcp_client.cpp
+++ TcpServerClient/ipv4_tcp_client.cpp
@@ -27,31 +27,33 @@
 
 #define BUFSIZE 1500
 
+struct sockaddr server_info;  // ソケット情報
+struct sockaddr_in *p_server; // ネットワーク用インターフェース
+struct hostent *host;
+socklen_t socket_length;
+unsigned short port_of_server = 54321;
+
+int socket_to_server; // サーバに接続するソケット
+char buf[BUFSIZE];
+
 int main(int argc, char **argv)
 {
     try
     {
-        struct sockaddr server_info;  // ソケット情報
-        struct sockaddr_in *p_server; // ネットワーク用インターフェース
-        struct hostent *host;
-        unsigned short port = 54321;
-
-        int sock_client;
-        char buf[BUFSIZE];
-
         /* 1.ソケットの作成 */
-        if ((sock_client = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        if ((socket_to_server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         {
             std::printf("[Error] %s\n", strerror(errno));
             throw std::runtime_error("socket");
         }
+        std::printf("[Done] Step1. create socket\n");
 
         /* 2.接続先指定用構造体の準備 */
-        p_server = (struct sockaddr_in *)&server_info;
+        p_server = (struct sockaddr_in *)&server_info; // `sockeaddr`構造体を`sockaddr_in*`構造体ポインタを通して使用する.
         p_server->sin_family = AF_INET;
-        p_server->sin_port = htons(port);
+        p_server->sin_port = htons(port_of_server);
 
-        /* 3.宛先の確定 */
+        /* 3.宛先(server)の確定 */
         char *server_name = "127.0.0.1";
         if ((p_server->sin_addr.s_addr = inet_addr(server_name)) == INADDR_NONE)
         {
@@ -65,30 +67,34 @@ int main(int argc, char **argv)
             p_server->sin_family = host->h_addrtype;
             std::memcpy(&(p_server->sin_addr), host->h_addr, host->h_length);
         }
+        std::printf("[Done] Step2. configure destination (server): `%s`; port=%u\n", server_name, port_of_server);
 
         /* 4.サーバーに接続 */
-        if (connect(sock_client, (struct sockaddr *)p_server, sizeof(struct sockaddr)) != 0)
+        socket_length = sizeof(server_info);
+        if (connect(socket_to_server, (struct sockaddr *)p_server, socket_length) != 0)
         {
             // 成功 0, 失敗 0以外
             std::printf("[Error] %s\n", strerror(errno));
             throw std::runtime_error("connect");
         }
+        std::printf("[Done] Step3. connect to server `%s`; port=%u\n", server_name, port_of_server);
 
         /* 5.サーバーに送信 */
         std::snprintf(buf, sizeof(buf), "message from IPv4 client");
-        int n = write(sock_client, buf, strnlen(buf, sizeof(buf)));
+        int n = write(socket_to_server, buf, strnlen(buf, sizeof(buf)));
 
         /* 6.サーバーから受信 */
         std::memset(buf, 0, sizeof(buf));
-        n = read(sock_client, buf, sizeof(buf));
+        n = read(socket_to_server, buf, sizeof(buf));
 
-        std::printf("n=%d, %s\n", n, buf);
+        std::printf("read n=%d, %s\n", n, buf);
 
         /* 7.ソケットを閉じる */
-        close(sock_client);
+        close(socket_to_server);
     }
     catch (const std::exception &e)
     {
+        close(socket_to_server);
         std::cerr << e.what() << '\n';
     }
 }
