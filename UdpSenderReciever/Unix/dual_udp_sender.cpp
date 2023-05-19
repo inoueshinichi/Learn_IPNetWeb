7f0dafd362ee2746bd3501a4be70a0a7f0b2876f
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-05-19 13:57:19 +0900

    [Update] make dual udp sender

---
+++ UdpSenderReciever/Unix/dual_udp_sender.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file dual_udp_sender.cpp
+ * @author Shinichi Inoue (inoue.shinichi.1800@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-05-18
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
